3cb123ff692fa9bba7737ce89305c285c7ef59d3
Mark-Oliver Junge <root@nyrox.dev>
2021-11-02 15:14:55 +0100

    Re-organization

--- core/gpu/bbox_intersect.fut
+++
@@ -1,8 +0,0 @@
-
-type Vec3 = (f32, f32, f32)
-type Ray = { origin: Vec3, direction: Vec3 }
-type BBox = {}
-
-entry bbox_intersect (x: []i32) (y: []i32): i32 =
-  reduce (+) 0 (map2 (*) x y)
-
--- raymond-core/src/lib.rs
+++ raymond-core/src/lib.rs
@@ -28,18 +28,3 @@ pub enum Material {
 	Emission(Vector3, Vector3, f64, f64),
 }
 
-use bbox_intersect::{Array_i32_1d, Error, FutharkContext};
-
-pub fn do_magic() -> Result<(), Error> {
-	let a = vec![1, 2, 3, 4];
-	let b = vec![2, 3, 4, 1];
-
-	let mut ctx = FutharkContext::new()?;
-	let a_arr = Array_i32_1d::from_vec(ctx, &a, &vec![4])?;
-	let b_arr = Array_i32_1d::from_vec(ctx, &b, &vec![4])?;
-
-	let res = ctx.bbox_intersect(a_arr, b_arr)?;
-
-	println!("{:?}", res);
-	return Ok(())
-}
